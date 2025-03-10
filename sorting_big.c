3538075660f8aef7cced8fb1f81fd20278799475
aboumata <boumatabibon@gmail.com>
2025-03-10 19:19:20 +0000

    push

--- sorting_big.c
+++ sorting_big.c
@@ -12,125 +12,75 @@
 
 #include "push_swap.h"
 
-static void print_stack(t_stack *stack, char *name)
-{
-	printf("%s: ", name);
-	while (stack)
-	{
-		printf("%d ", stack->val);
-		stack = stack->next;
+void assign_indexes(t_stack *stack) {
+	t_stack *cur;
+	int *arr;
+	int size, i;
+
+	size = stack_len(stack);
+	arr = malloc(size * sizeof(int));
+	if (!arr)
+		return;
+
+	cur = stack;
+	i = 0;
+	while (cur) {
+		arr[i++] = cur->val;
+		cur = cur->next;
 	}
-	printf("\n");
+	quicksort(arr, 0, size - 1);
+	cur = stack;
+	while (cur) {
+		cur->index = binary_search(arr, size, cur->val);
+		cur = cur->next;
+	}
+
+	free(arr);
 }
 
-#include <stdlib.h>
-
-void	assign_indexes(t_stack *stack)
+void chunk_sort(t_stack **a, t_stack **b, int chunk_size)
 {
-	t_stack	*current;
-	int		*array;
-	int		size;
-	int		i;
-	int		j;
-	int		temp;
-
-	size = 0;
-	current = stack;
-	while (current)
+	int chunk_num = (stack_len(*a) + chunk_size - 1) / chunk_size;
+	for (int i = 0; i < chunk_num; i++)
 	{
-		size++;
-		current = current->next;
-	}
-	array = (int *)malloc(size * sizeof(int));
-	if (!array)
-		return ;
-	current = stack;
-	i = 0;
-	while (current)
-	{
-		array[i] = current->val;
-		i++;
-		current = current->next;
-	}
-	i = 0;
-	while (i < size - 1)
-	{
-		j = 0;
-		while (j < size - i - 1)
+		int min = i * chunk_size;
+		int max = (i + 1) * chunk_size;
+		while (stack_len(*a) > 0)
 		{
-			if (array[j] > array[j + 1])
-			{
-				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
-			}
-			j++;
+			if ((*a)->index >= min && (*a)->index < max)
+				push_stack(b, a, 'b');
+			else
+				rotate_stack(a, 'a');
 		}
-		i++;
 	}
-	current = stack;
-	while (current)
-	{
-		i = 0;
-		while (i < size)
-		{
-			if (current->val == array[i])
-			{
-				current->index = i;
-				break ;
-			}
-			i++;
-		}
-		current = current->next;
-	}
-
-	free(array);
+	while (*b)
+		push_stack(a, b, 'a');
 }
 
-static void process_bit(t_stack **src, t_stack **dst, int bit, char stack_name)
+static void process_bit(t_stack **a, t_stack **b, int bit)
 {
-	int initial_size = stack_len(*src);
-	int processed = 0;
-
-	while (processed < initial_size)
+	int size = stack_len(*a);
+	int i = 0;
+	while (i++ < size)
 	{
-		printf("Processing bit %d: %d\n", bit, (*src)->index);
-		if ((((*src)->index >> bit) & 1) == 0)
-		{
-			printf("Pushing from %c to %c: %d\n", stack_name, stack_name == 'a' ? 'b' : 'a', (*src)->val);
-			push_stack(dst, src, stack_name == 'a' ? 'b' : 'a');
-		}
+		if ((((*a)->index >> bit) & 1) == 0)
+			push_stack(b, a, 'b');
 		else
-		{
-			printf("Rotating %c: %d\n", stack_name, (*src)->val);
-			rotate_stack(src, stack_name);
-		}
-		processed++;
+			rotate_stack(a, 'a');
 	}
-	print_stack(*src, stack_name == 'a' ? "Stack A" : "Stack B");
-	print_stack(*dst, stack_name == 'a' ? "Stack B" : "Stack A");
 }
 
 void radix_sort(t_stack **a, t_stack **b)
 {
 	int max_bits = 0;
-	int bit;
-	int size;
-
-	assign_indexes(*a);
-	size = stack_len(*a);
-	while ((size - 1) >> max_bits)
+	int max_num = stack_len(*a) - 1;
+	while ((max_num >> max_bits) != 0)
 		max_bits++;
-	print_stack(*a, "Stack A");
 
-	bit = 0;
-	while (bit < max_bits)
+	for (int bit = 0; bit < max_bits; bit++)
 	{
-		printf("Processing bit level: %d\n", bit);
-		process_bit(a, b, bit, 'a');
+		process_bit(a, b, bit);
 		while (*b)
 			push_stack(a, b, 'a');
-		bit++;
 	}
-	print_stack(*a, "Final Stack A");
 }
